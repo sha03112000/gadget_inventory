d8d79bd44b39f4adeaa96c411c32536bb38f574c
Shabad <shabadmk969@outlook.com>
2025-11-27 05:36:22 +0530

    rendel url has been changed

--- frontend/src/constants/baseUrl.ts
+++ frontend/src/constants/baseUrl.ts
@@ -1,3 +1,3 @@
-// export const baseUrl = 'http://192.168.20.6:4000/api'
+export const baseUrl = 'https://api-gadget-inventory.onrender.com/api'
 
-export const baseUrl = 'http://localhost:4000/api'
+// export const baseUrl = 'http://localhost:4000/api'
